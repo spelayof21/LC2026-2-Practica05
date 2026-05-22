9f6536d64a979355b9d43a8114df6cecb9925c35
spelayof21 <spelayof@ciencias.unam.mx>
2026-05-21 21:42:40 -0600

    ya esta la practica

--- README.md
+++ README.md
@@ -22,13 +22,5 @@ Si no les llegan a funcionar, es posible que el problema es que tengan una versi
 
 En esta sección deben eliminar esta línea de texto, borrar la leyenda "Integrante n" y escribir su nombre empezando por apellidos y su número de cuenta.
 
-+ Integrante 1
-    - No. de Cuenta: 
-+ Integrante 2
-    - No. de Cuenta: 
-+ Integrante 3
-    - No. de Cuenta: 
-
-## Comentarios
-
-Recuerden que opcionalmente pueden agregar un comentario (ver los lineamientos de entrega). Si es el caso, borren esta línea y dejen su comentario.
++ Salvador Pelayo Fuentes
+    - No. de Cuenta: 321233196
--- src/Practica05.hs
+++ src/Practica05.hs
@@ -2,32 +2,88 @@ module Practica05 where
 
 import Terminos
 
---Aplicar una sustitucion a un termino
+-- Aplicar una sustitución a un término
 apsubT :: Term -> Subst -> Term
-apsubT = undefined 
+apsubT (Var x) s =
+  case lookup x s of
+    Just t  -> t
+    Nothing -> Var x
+apsubT (Fun f args) s = Fun f (aplicarLista args s)
 
---Funcion auxiliar para aplicar la sustitucion a una lista de terminos
+-- Función auxiliar para aplicar la sustitución a una lista de términos
 aplicarLista :: [Term] -> Subst -> [Term]
-aplicarLista = undefined 
+aplicarLista ts s = map (`apsubT` s) ts
 
---Funcion que elimina los pares que son de la forma x=x
+-- Función que elimina los pares que son de la forma x=x
 simpSus :: Subst -> Subst
-simpSus = undefined
+simpSus = filter (\(x, t) -> t /= Var x)
 
 --Funcion que calcula la composicion de dos sustituciones
 compSus :: Subst -> Subst -> Subst
-compSus = undefined
+compSus s1 s2 =
+  let s1Aplicada = map (\(x, t) -> (x, apsubT t s2)) s1
+      dominioS1  = map fst s1
+      s2Nueva    = filter (\(x, _) -> x `notElem` dominioS1) s2
+  in simpSus (s1Aplicada ++ s2Nueva)
 
---Funcion que devuelve un umg de dos terminos, si es que lo hay
+-- Verifica si una variable x ocurre en un término
+ocurre :: Nombre -> Term -> Bool
+ocurre x (Var y)     = x == y
+ocurre x (Fun _ args) = any (ocurre x) args
+
+-- Funcion que devuelve un umg de dos terminos, si es que lo hay
 unifica :: Term -> Term -> [Subst]
-unifica = undefined
+-- Caso: dos variables iguales
+unifica (Var x) (Var y)
+  | x == y    = [[]]
+  | otherwise = [[( x, Var y)]]
 
+-- Caso: variable con término
+unifica (Var x) t
+  | ocurre x t = []
+  | otherwise  = [[(x, t)]]
 
---Funcion que devuelve un unificador de dos términos funcionales, si es que lo hay
+-- Caso: término con variable
+unifica t (Var x)
+  | ocurre x t = []
+  | otherwise  = [[(x, t)]]
+
+-- Caso: dos constantes
+unifica (Fun f []) (Fun g [])
+  | f == g    = [[]]
+  | otherwise = []
+
+-- Caso: dos términos funcionales
+unifica (Fun f args1) (Fun g args2)
+  | f /= g                    = []
+  | length args1 /= length args2 = []
+  | otherwise                 = unificaListas args1 args2
+
+-- Funcion que devuelve un unificador de dos terminos funcionales, si es que lo hay
 unificaListas :: [Term] -> [Term] -> [Subst]
-unificaListas = undefined
+unificaListas [] [] = [[]]
+unificaListas (t1:ts1) (t2:ts2) =
+  case unifica t1 t2 of
+    []    -> []
+    [mu1] ->
+      let ts1' = aplicarLista ts1 mu1
+          ts2' = aplicarLista ts2 mu1
+      in case unificaListas ts1' ts2' of
+           []    -> []
+           [mu2] -> [compSus mu1 mu2]
+    _     -> []
+unificaListas _ _ = []
 
---Funcion que devuelve un umg de una lista de termino, si es que lo hay
+-- Funcion que devuelve un umg de una lista de termino, si es que lo hay
 unificaConj :: [Term] -> [Subst]
-unificaConj = undefined
-
+unificaConj []  = [[]]
+unificaConj [_] = [[]]
+unificaConj (t1:t2:ts) =
+  case unifica t1 t2 of
+    []    -> []
+    [mu1] ->
+      let ts' = aplicarLista (t2:ts) mu1
+      in case unificaConj ts' of
+           []    -> []
+           [mu2] -> [compSus mu1 mu2]
+    _     -> []
